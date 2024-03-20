9038ab9ea86b9318710470b32ae7e22d33fdd7a9
paul-loedige <59517210+ploedige@users.noreply.github.com>
2024-03-20 13:15:41 +0100

    added old scripts (issue #1)

---
+++ scripts/start_gripper_202.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash -l
+
+if [[ "$CONDA_DEFAULT_ENV" != "polymetis" ]]
+then
+    echo Activating polymetis env
+
+    eval "$(conda shell.bash hook)"
+    conda activate polymetis
+
+    if [[ "$CONDA_DEFAULT_ENV" != "polymetis" ]]
+    then
+        echo Failed to activate conda environment.
+        exit 1
+    fi
+fi
+
+launch_gripper.py gripper=franka_hand ip=10.10.10.210 gripper.executable_cfg.robot_ip=10.10.10.202
