190ce8420a948572ba7bbee659b19c4c28ef75c2
ploedige <59517210+ploedige@users.noreply.github.com>
2024-06-03 16:44:33 +0200

    added client start to server interface. Loading Hydra config still missing

--- polymetis/python/polymetis/server_interface.py
+++ polymetis/python/polymetis/server_interface.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 import logging
 import os
 import subprocess
-import sys
 import signal
+import time
+import hydra
 
 from polymetis.utils.grpc_utils import check_server_exists
 from polymetis.utils.data_dir import BUILD_DIR, which
@@ -12,10 +13,12 @@ class BaseServerInterface(ABC):
     def __init__(self, 
                  name: str,
                  ip: str, 
-                 port: int):
+                 port: int,
+                 timeout: int = 15):
         self.name = name
         self.ip = ip
         self.port = port
+        self.timeout = timeout
 
         self.logger = logging.getLogger(self.name)
         self.pgid = None
@@ -36,12 +39,13 @@ class RobotServerInterface(BaseServerInterface):
                  ip:str, 
                  port:int, 
                  robot_name:str,
+                 timeout:int = 15,
                  use_real_time: bool = True,
                  robot_model="franka_panda",
                  robot_client="franka_hardware",
                  server_exec="run_server"):
         name = f"{robot_name}_server"
-        super().__init__(name, ip, port)
+        super().__init__(name, ip, port, timeout=timeout)
         self.use_real_time = use_real_time
         self.robot_model = robot_model
         self.robot_client = robot_client
@@ -59,18 +63,31 @@ class RobotServerInterface(BaseServerInterface):
             "To prevent undefined behavior, start the service on a different port or "
             "kill stale servers with 'pkill -9 run_server'"
         )
+
+        # start server
         self.logger.info(f"Starting server...")
         server_exec_path = which(self.server_exec)
         server_cmd = [server_exec_path]
-        server_cmd = server_cmd + ["-s", self.ip, "-p", self.port]
+        server_cmd = server_cmd + ["-s", self.ip, "-p", str(self.port)]
 
         if self.use_real_time:
             server_cmd += ["-r"]
 
         server_output = subprocess.Popen(
-            server_cmd, stdout=sys.stdout, stderr=sys.stderr, preexec_fn=os.setpgrp
+            server_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True
         )
         self.pgid = os.getpgid(server_output.pid)
+
+        # start client
+        t0 = time.time()
+        while not check_server_exists(self.ip, self.port):
+            time.sleep(0.1)
+            if time.time() - t0 > self.timeout:
+                raise ConnectionError("Robot client: Unable to locate server.")
+        self.logger.info("Starting robot client...")
+        client = hydra.utils.instantiate(self.robot_client)
+        client.run()
+
 
         return server_output
 
