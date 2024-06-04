71ac746fa36d112893a4fdbd97c8aef230c3effc
paul-loedige <59517210+ploedige@users.noreply.github.com>
2024-06-04 14:30:25 +0200

    removed server interface

--- polymetis/python/polymetis/__init__.py
+++ polymetis/python/polymetis/__init__.py
@@ -4,5 +4,4 @@
 # LICENSE file in the root directory of this source tree.
 from ._version import __version__
 from .robot_interface import RobotInterface
-from .server_interface import RobotServerInterface
 from .gripper_interface import GripperInterface
